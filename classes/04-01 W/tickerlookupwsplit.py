c3190644621efa4eca9a1fcdc90bd0d2b15d277d
student <student@example.com>
2026-04-06 10:12:15 -0400

    class 4/6

---
+++ classes/04-01 W/tickerlookupwsplit.py
@@ -0,0 +1,20 @@
+{
+  "NFLX": {
+    "date": "2025-11-17",
+    "action": "stock_split",
+    "new_shares": 10,
+    "old_shares": 1
+  }
+}
+
+#if fraction 
+
+'''
+from fractions import Fraction
+
+ratio = 1.5
+f = Fraction(ratio).limit_denominator()
+
+new_shares = f.numerator
+old_shares = f.denominator
+'''
